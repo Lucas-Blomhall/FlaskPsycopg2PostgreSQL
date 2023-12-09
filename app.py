9ca2764fb8e344100fe7cf71cdaad868d0a146af
Lucas Blomhäll <lucas.blomhall@gmail.com>
2023-12-09 12:03:57 +0100

    update put created

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from database import create_category, create_listing, view_listing
+from database import create_category, create_listing, update_listing, view_customer, view_listing, delete_listing, view_listing_by_id
 
 app = Flask(__name__)
 
@@ -9,7 +9,8 @@ def get_todos():
     return jsonify({'message': "welcome to hemnet!"})
 
 
-# Vi kör en foreach loop där vi kollar igenom alla värdena
+# Vi kör en foreach loop där vi kollar igenom alla värdena för  att kunna formatera värderna
+# listing
 def format_listing(listings):
     return [{
         'id': listing['id'],
@@ -20,18 +21,24 @@ def format_listing(listings):
         'broker_id': listing['broker_id']
     } for listing in listings]
 
-
-# Get all listings
-@app.route('/listing', methods=['GET'])
-def get_listing_route():
-    listings = view_listing()
-    new_listing = format_listing(listings)
-    return jsonify(new_listing), 200
+# customer
 
 
-# Create a listing
+def format_customer(customers):
+    return [{
+        'id': customer['id'],
+        'name': customer['name'],
+        'email': customer['email'],
+        'contact_info': customer['contact_info']
+    } for customer in customers]
+
+
+# Create listing
 @app.route('/listing', methods=['POST'])
-def create_listing_route():
+def add_listing_route():
+    """
+    Endpoint to add a new listing
+    """
     data = request.get_json()
     name = data['name']
     price = data['price']
@@ -40,6 +47,199 @@ def create_listing_route():
     broker_id = data['broker_id']
     create_listing(name, price, description, category_id, broker_id)
     return jsonify({'message': "created listing successfully"}), 201
+
+
+# Read listing
+@app.route('/listing/<int:id>', methods=['GET'])
+def listing_detail_route(id):
+    """
+    Endpoint to return a specific listing
+    """
+    selected_listing = view_listing_by_id(id)
+    new_listing = format_listing(selected_listing)
+    return jsonify(new_listing), 200
+
+
+# Read listing
+@app.route('/listing', methods=['GET'])
+def get_all_listings_route():
+    """
+    Should return a list of X number of listings based on a LIMIT
+    """
+    listings = view_listing()
+    new_listing = format_listing(listings)
+    return jsonify(new_listing), 200
+
+
+# Update listing
+@app.route('/listing/<int:id>', methods=['PUT'])
+def update_listing_route(id):
+    """
+    Endpoint to update an existing listing
+    """
+    data = request.get_json()  # Hämtar data från Postman
+    name = data['name']
+    price = data['price']
+    description = data['description']
+    category_id = data['category_id']
+    broker_id = data['broker_id']
+
+    success = update_listing(
+        id, name, price, description, category_id, broker_id)
+
+    if success == True:
+        return jsonify({'message': "Updated listing successfully"}), 200
+    else:
+        return jsonify({'message': "Failed to update listing"}), 400
+
+
+# Delete listing
+@app.route('/listing/<id>', methods=['DELETE'])
+def remove_listing_route(id):
+    """
+    Endpoint to remove a listing
+    """
+    delete_listing(id)
+    return jsonify({'message': "deleted listing successfully"}), 200
+
+
+@app.route('/category', methods=['POST'])
+def add_category_route():
+    """
+    Endpoint to add a new category
+    Get category details from customer and call db.create_category
+    """
+    data = request.get_json()
+    name = data['name']
+    create_category(name)
+    return jsonify({'message': "created listing successfully"}), 201
+
+
+def add_category():
+
+    pass
+
+
+@app.route('/category/<int:id>', methods=['PUT'])
+def update_category_route():
+    """
+    Endpoint to update a category
+    Get updated category details from customer and call db.update_category
+    """
+    data = request.get_json()
+
+    selected_category = view_listing_by_id(id)
+    new_category = format_listing(selected_category)
+    return jsonify(new_category), 200
+
+
+# # Update single listing
+# @app.route('/listing/<int:id>', methods=['PUT'])
+# def update_listing_route(id):
+#     data = request.get_json()
+#     selected_listing = view_listing_by_id(id)
+#     new_listing = format_listing(selected_listing)
+#     return jsonify(new_listing), 200
+
+
+def add_broker():
+    """
+    Endpoint to add a new broker
+    Get broker details from customer and call db.create_broker
+    """
+    pass
+
+
+def modify_broker():
+    """
+    Endpoint to modify a broker
+    Get updated broker details from customer and call db.update_broker
+    """
+    pass
+
+
+def add_customer():
+    """
+    Endpoint to add a new customer
+    Get customer details from customer and call db.create_customer
+    """
+    pass
+
+
+def remove_customer():
+    """
+    Endpoint to remove a customer
+    Get customer ID from customer and call db.delete_customer
+    """
+    pass
+
+
+@app.route('/customer', methods=['GET'])
+def get_all_customer_route():
+    """
+    Endpoint to list all customer
+    """
+    customers = view_customer()
+    new_customer = format_customer(customers)
+    return jsonify(new_customer), 200
+
+
+def schedule_appointment():
+    """
+    Endpoint to schedule a viewing appointment
+    Get appointment details from customer and call db.create_appointment
+    """
+    pass
+
+
+def update_appointment():
+    """
+    Endpoint to update an existing appointment
+    Get appointment details from customer and call db.update_appointment
+    """
+    pass
+
+
+def cancel_appointment():
+    """
+    Endpoint to cancel an appointment
+    Get appointment ID from customer and call db.remove_appointment
+    """
+    pass
+
+
+def list_customer_appointments():
+    """
+    Endpoint that returns appointments for a specific customer
+    Get customer ID from customer and call db.view_appointments_for_customer
+    """
+    pass
+
+
+def favorite_listing():
+    """
+    Endpoint to let a customer favorite a specific listing
+    Should ideally only need a title, it's your choice how to implement it
+    """
+    pass
+
+
+def unfavorite_listing():
+    """
+    Endpoint to let a customer unfavorite a specific listing
+    Should ideally only need a title, but it's your choice how to implement it
+    """
+    pass
+
+
+# # Update single listing
+# @app.route('/listing/<int:id>', methods=['PUT'])
+# def update_listing_route(id):
+#     data = request.get_json()
+
+#     selected_listing = view_listing_by_id(id)
+#     new_listing = format_listing(selected_listing)
+#     return jsonify(new_listing), 200
 
 
 # category routes:
@@ -56,25 +256,3 @@ def create_category_route():
 # app.run(debug=True) default way
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)  # bättre sätt
-
-
-# url = "postgresql://postgres:Vanligt123!@localhost/carappdb"
-# conn = psycopg2.connect(url)
-
-# '''If everything works fine you will get a
-# message that Flask is working on the first
-# page of the application
-# '''
-
-
-# @app.route('/')
-# def check():
-#     return 'Flask is working'
-
-
-# @app.route('/connect')
-# def index():
-#     with conn:
-#         conn = psycopg2.connect(
-#             "postgresql://postgres:root@localhost:5432/postgres")
-#         return 'it works'
--- database.py
+++ database.py
@@ -2,12 +2,20 @@
 import psycopg2
 import psycopg2.extras
 from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Database connection
-database_url = "postgresql://postgres:Vanligt123!@localhost/hemnet"
-client = psycopg2.connect(database_url)
-db = client.cursor(cursor_factory=psycopg2.extras.DictCursor)
+database_url = os.getenv("DATABASE_URL")
 
+
+# jag gillar det här sättet men vi stänger inte våran cursor och det är inte så bra. Den används ändå i main.py?
+# client = psycopg2.connect(database_url)
+# db = client.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+def get_db_connection():
+    return psycopg2.connect(database_url, cursor_factory=psycopg2.extras.DictCursor)
 
 # Create table
 
@@ -24,71 +32,81 @@ def create_tables():
     Create any necessary tables in this function, you can choose yourself if you want to
     run it at start or not.
     """
-    create_broker = """
-    CREATE TABLE IF NOT EXISTS broker(
-    id SERIAL PRIMARY KEY
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) CHECK(email LIKE '%@%') UNIQUE,
-    contact_info VARCHAR(20)
-    );
-    """
-    create_category = """
-    CREATE TABLE IF NOT EXISTS category(
-    id SERIAL PRIMARY KEY
-    name VARCHAR(255) UNIQUE NOT NULL,
-    );
-    """
-    create_customer = """
-    CREATE TABLE IF NOT EXISTS customer(
-    id SERIAL PRIMARY KEY
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) CHECK(email LIKE '%@%') UNIQUE,
-    contact_info VARCHAR(20)
-    );
-    """
-    create_listing = """
-    CREATE TABLE IF NOT EXISTS listing(
-    id SERIAL PRIMARY KEY
-    name VARCHAR(255) NOT NULL,
-    price INT NOT NULL,
-    description VARCHAR(255),
-    category_id INT REFERENCES category(id),
-    broker_id INT REFERENCES category(id)    
-    );
-    """
-    create_listing_customer = """
-    CREATE TABLE IF NOT EXISTS listing_customer(
-    listing_id INT,
-    customer_id INT,
-    appointments BOOLEAN,
-    favorite_residence BOOLEAN
-    PRIMARY KEY(listing_id, customer_id)
-    );
-    """
-    db.execute(create_broker)
-    db.execute(create_category)
-    db.execute(create_customer)
-    db.execute(create_listing)
-    db.execute(create_listing_customer)
-    client.commit()
-    print("Created table successfully in PostgreSQL")
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            create_broker = """
+            CREATE TABLE IF NOT EXISTS broker(
+            id SERIAL PRIMARY KEY
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) CHECK(email LIKE '%@%') UNIQUE,
+            contact_info VARCHAR(20)
+            );
+            """
+            create_category = """
+            CREATE TABLE IF NOT EXISTS category(
+            id SERIAL PRIMARY KEY
+            name VARCHAR(255) UNIQUE NOT NULL,
+            );
+            """
+            create_customer = """
+            CREATE TABLE IF NOT EXISTS customer(
+            id SERIAL PRIMARY KEY
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) CHECK(email LIKE '%@%') UNIQUE,
+            contact_info VARCHAR(20)
+            );
+            """
+            create_listing = """
+            CREATE TABLE IF NOT EXISTS listing(
+            id SERIAL PRIMARY KEY
+            name VARCHAR(255) NOT NULL,
+            price INT NOT NULL,
+            description VARCHAR(255),
+            category_id INT REFERENCES category(id),
+            broker_id INT REFERENCES category(id)    
+            );
+            """
+            create_listing_customer = """
+            CREATE TABLE IF NOT EXISTS listing_customer(
+            listing_id INT,
+            customer_id INT,
+            appointments BOOLEAN,
+            favorite_residence BOOLEAN
+            PRIMARY KEY(listing_id, customer_id)
+            );
+            """
+            try:
+                cur.execute(create_broker)
+                cur.execute(create_category)
+                cur.execute(create_customer)
+                cur.execute(create_listing)
+                cur.execute(create_listing_customer)
+                # extra table för favorit
+                conn.commit()
+                print("Created table successfully in PostgreSQL")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
 
 
 # Check if the table exists
 def check_if_exists(table_name):
-    try:
-        db.execute("""
-                    SELECT EXISTS(
-                    SELECT 1 FROM information_schema.tables
-                    WHERE table_catalog = 'hemnet'
-                    AND table_schema = 'public'
-                    AND table_name = %s
-                    );
-                    """, (table_name,))
-        return db.fetchone()[0]
-    except psycopg2.Error as e:
-        print("Error", e)
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute("""
+                            SELECT EXISTS(
+                            SELECT 1 FROM information_schema.tables
+                            WHERE table_catalog = 'hemnet'
+                            AND table_schema = 'public'
+                            AND table_name = %s
+                            );
+                            """, (table_name,))
+                return cur.fetchone()[0]
+            except psycopg2.Error as e:
+                print("Error", e)
+                return False
 
 
 print(f"Table {table_name} exists: {check_if_exists(table_name)}")
@@ -108,42 +126,87 @@ print(f"Table {table_name} exists: {check_if_exists(table_name)}")
 def create_listing(name, price, description, category_id, broker_id):
     """Creates a new listing in the database."""
     # Implement the SQL query to insert a new listing
-    try:
-        db.execute(
-            "INSERT INTO listing (name, price, description, category_id, broker_id) VALUES (%s, %s, %s, %s, %s)", (name, price, description, category_id, broker_id))
-        client.commit()
-        print("Created listing successfully")
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        client.rollback()
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "INSERT INTO listing (name, price, description, category_id, broker_id) VALUES (%s, %s, %s, %s, %s)", (name, price, description, category_id, broker_id))
+                conn.commit()
+                print("Created listing successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
 
 
 # Delete a listing
 def delete_listing(id):
     """Deletes a listing from the database."""
-    try:
-        db.execute(
-            "DELETE FROM listing WHERE id = %s", (id,))
-        client.commit()
-        print("Deleted listing successfully")
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        client.rollback()
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "DELETE FROM listing WHERE id = %s", (id,))
+                conn.commit()
+                print("Deleted listing successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
 
 
 # Get a listing
 def view_listing():
     """Retrieves details of a specific listing along with category and broker information."""
     # Implement the SQL query to retrieve listing details with JOIN
-    try:
-        db.execute("SELECT * FROM listing ORDER BY id ASC")
-        print("Got all listing successfully")
-        return db.fetchall()  # Den returnar alla todos
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute("SELECT * FROM listing ORDER BY id ASC")
+                print("Got all listing successfully")
+                return cur.fetchall()  # Den returnar alla todos
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                return False
+
+
+# Update a listing by id
+def update_listing(id, name, price, description, category_id, broker_id):
+    """Updates an existing listing."""
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "UPDATE listing SET name = %s, price = %s, description = %s, category_id = %s, broker_id = %s WHERE id = %s",
+                    (name, price, description, category_id, broker_id, id))
+                conn.commit()
+                print("Updated listing by id successfully")
+                return True
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
+
+
+# # Get a listing by id kommer tillbaka och fixar för specifik id
+# id SERIAL PRIMARY KEY
+# name VARCHAR(255) NOT NULL,
+# price INT NOT NULL,
+# description VARCHAR(255),
+# category_id INT REFERENCES category(id),
+# broker_id INT REFERENCES category(id)
+def view_listing_by_id(id):
+    """Retrieves details of a specific listing along with category and broker information."""
+    # Implement the SQL query to retrieve listing details with JOIN
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute("SELECT * FROM listing WHERE id = %s", (id,))
+                print("Got specific listing by id successfully")
+                return cur.fetchall()  # Den returnar alla todos
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                return False
 
 
 # Category: CRUD
@@ -151,28 +214,32 @@ def view_listing():
 # Create Category
 def create_category(name):
     """Creates a new category in the database."""
-    try:
-        db.execute(
-            "INSERT INTO category (name) VALUES (%s)", (name,))
-        client.commit()
-        print("Created category successfully")
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        client.rollback()
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "INSERT INTO category (name) VALUES (%s)", (name,))
+                conn.commit()
+                print("Created category successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
 
 
 # Update a category by id
 def update_category(id, name):
     """Updates an existing category."""
-    try:
-        db.execute(
-            "Update category SET title = %s, description = %s, WHERE id = %s", (id, name))
-        client.commit()
-        print("Updated category by id successfully")
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "Update category SET title = %s, description = %s, WHERE id = %s", (id, name))
+                conn.commit()
+                print("Updated category by id successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                return False
 
 
 # broker: CRUD
@@ -180,77 +247,139 @@ def update_category(id, name):
 # Create a broker
 def create_broker(name, email, contact_info):
     """Creates a new broker in the database."""
-    try:
-        db.execute(
-            "INSERT INTO broker (name, email, contact_info) VALUES (%s, %s, %s)", (name, email, contact_info))
-        client.commit()
-        print("Created category successfully")
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        client.rollback()
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "INSERT INTO broker (name, email, contact_info) VALUES (%s, %s, %s)", (name, email, contact_info))
+                conn.commit()
+                print("Created category successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
 
 
 # Update a broker by id
 def update_broker(id, name, email, contact_info):
     """Updates an existing broker."""
-    try:
-        db.execute(
-            "Update broker SET name = %s, email = %s, contact_info = %s, WHERE id = %s", (id, name, email, contact_info))
-        client.commit()
-        print("Updated category by id successfully")
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "Update broker SET name = %s, email = %s, contact_info = %s, WHERE id = %s", (id, name, email, contact_info))
+                conn.commit()
+                print("Updated category by id successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                return False
 
 
 # create a customer
 def create_customer(name, email, contact_info):
     """Creates a new customer in the database."""
-    try:
-        db.execute(
-            "INSERT INTO customer (name, email, contact_info) VALUES (%s, %s, %s)", (name, email, contact_info))
-        client.commit()
-        print("Created customer successfully")
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        client.rollback()
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "INSERT INTO customer (name, email, contact_info) VALUES (%s, %s, %s)", (name, email, contact_info))
+                conn.commit()
+                print("Created customer successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
+
+
+# Read customer
+def view_customer():
+    """Retrieves details of a specific listing along with category and broker information."""
+    # Implement the SQL query to retrieve listing details with JOIN
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute("SELECT * FROM customer ORDER BY id ASC")
+                print("Got all listing successfully")
+                return cur.fetchall()  # Den returnar alla todos
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                return False
+
+# UPDATE customer
 
 
 def delete_customer(id):
     """Deletes a customer from the database."""
-    try:
-        db.execute(
-            "DELETE FROM customer WHERE id = %s", (id,))
-        client.commit()
-        print("Deleted customer successfully")
-    except psycopg2.Error as e:
-        print("Error: ", e)
-        client.rollback()
-        return False
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "DELETE FROM customer WHERE id = %s", (id,))
+                conn.commit()
+                print("Deleted customer successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
 
 
-def create_appointment(connection):
+def create_appointment(listing_id, customer_id, appointments):
     """Creates a new viewing appointment."""
-    pass
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "INSERT INTO listing_customer (listing_id, customer_id, appointments) VALUES (%s, %s)", (
+                        listing_id, customer_id, appointments)
+                )
+                conn.commit()
+                print("Created an appointment in listing_customer successfully")
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                conn.rollback()
+                return False
 
 
-def remove_appointment(connection):
+def remove_appointment(listing_id, customer_id):
     """Removes an appointment."""
-    pass
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "DELETE FROM listing_customer WHERE listing_id = %s AND customer_id = %s", (listing_id, customer_id))
+                print("Deleted an appointment from listing_customer")
+                return cur.fetchall()  # Den returnar alla todos
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                return False
 
 
-def update_appointment(connection):
-    """
-    Updates an appointment
-    """
-    pass
+def update_appointment(listing_id, customer_id, appointments):
+    """Updates an appointment"""
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "Update listing_customer SET appointments = %s, WHERE listing_id = %s AND customer_id = %s", (listing_id, customer_id, appointments))
+                print("Update an appointment from listing_customer")
+                return cur.fetchall()  # Den returnar alla todos
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                return False
 
 
-def view_appointments_for_customer(connection):
+def view_appointments_for_customer(listing_id, customer_id):
     """Retrieves all appointments for a specific customer."""
-    pass
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            try:
+                cur.execute(
+                    "SELECT * FROM listing_customer WHERE customer_id = %s", (listing_id, customer_id))
+                print("Update an appointment from listing_customer")
+                return cur.fetchall()  # Den returnar alla todos
+            except psycopg2.Error as e:
+                print("Error: ", e)
+                return False
 
 
 def favorite_listing(connection):
